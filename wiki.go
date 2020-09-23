979136c9c647933a828ebefe77b8aa0941865114
mercimat <11376662+mercimat@users.noreply.github.com>
2020-09-23 21:39:08 +0200

    display hostname for docker swarm lab

--- wiki.go
+++ wiki.go
@@ -18,8 +18,15 @@ type Page struct {
     Body  []byte `json:”body,omitempty”`
 }
 
+type TemplateData struct {
+    Title string
+    Body  []byte
+    Host  string
+}
+
 var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var hostname string
 
 // mongodb global vars
 var collection *mongo.Collection
@@ -65,7 +72,7 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-    err := templates.ExecuteTemplate(w, tmpl+".html", p)
+    err := templates.ExecuteTemplate(w, tmpl+".html", TemplateData{p.Title, p.Body, hostname})
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
@@ -115,6 +122,7 @@ func frontPageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    hostname, _ = os.Hostname()
     InitDB()
     http.HandleFunc("/", frontPageHandler)
     http.HandleFunc("/view/", makeHandler(viewHandler))
