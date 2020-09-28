1c23a4623113600c31fab59342510822dddfedfc
mercimat <11376662+mercimat@users.noreply.github.com>
2020-09-28 23:33:56 +0200

    update run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 export PATH=$PATH:/usr/local/go/bin
+export GOPATH="$WORKSPACE"
+
+mkdir -p $GOPATH/src/github.com/mercimat
+ln -s $WORKSPACE $GOPATH/src/github.com/mercimat/wikiapp
+
 go get -d -v go.mongodb.org/mongo-driver/mongo
-go test -v -vet=off . -run TestFrontPageHandler
+
+cd web/
+go test -v -vet=off .
